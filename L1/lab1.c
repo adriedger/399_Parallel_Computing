246601bae4d2b7abb6cc9b258c2447f50c0208bb
Andre Driedger <driedgera2@cc5-212-18.macewan.ca>
2015-09-17 13:33:05 -0600

    final commit lab1

---
+++ L1/lab1.c
@@ -0,0 +1,55 @@
+// Andre Driedger 1805536
+// cmpt399 Lab1 Pthreads
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define THRDS 2
+
+int counter = 0;
+
+void *printHello(void *threadid){
+	long tid;
+	tid = (long)threadid;
+	printf("Hey! I'm thread #%ld!\n", tid);
+	int i;
+	for(i=0; i<1000000; i++){
+//		printf("Addin'\n");
+		counter ++;
+		}
+//	printf("Counter Total is %d\n", counter);
+	pthread_exit(NULL);
+}
+
+int main(){
+	printf("addr of main: %llx\n", (unsigned long long int)main);
+	pthread_t threads[THRDS];
+	pthread_attr_t attr;
+	int rc; 
+	long t;
+	void *status;
+
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+	for(t=0; t<THRDS; t++){
+		printf("Creating thread #%ld\n", t);
+		rc = pthread_create(&threads[t], NULL, printHello, (void*)t);
+		if(rc){
+			printf("ERROR: RETURN CODE FROM PTHREAD_CREATE() IS %d\n", rc);
+			exit(-1);
+		}
+	}
+	pthread_attr_destroy(&attr);
+	for(t=0; t<THRDS; t++){
+		rc = pthread_join(threads[t], &status);
+		if(rc){
+			printf("ERROR: RETURN CODE FROM PTHREAD_JOIN() IS %d\n", rc);
+			exit(-1);
+		}
+		printf("Completed join with thread %ld having status %ld\n", t, (long)status);
+	}
+	printf("Counter Total is %d\n", counter);
+	pthread_exit(NULL);
+}
