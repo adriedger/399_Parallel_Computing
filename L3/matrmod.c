258f5fc315496a4c209b6afeb219f58a6e037b0f
Andre Driedger <driedgera2@cc5-212-18.macewan.ca>
2015-10-15 11:27:05 -0600

    lab 3 makefile, matmulti algorith

---
+++ L3/matrmod.c
@@ -0,0 +1,129 @@
+// Andre Driedger 1805536
+// CMPT399 A1 Matrix Product
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+//int A[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+//int B[] = {1, 0, 0, 0 ,1 ,0 ,0 ,0, 1};
+//int AB[9];
+//double mSize = sqrt(sizeof(A)/sizeof(A[0]));
+
+double *A; 
+double *B;
+double *AB;
+int LENGTH, THRDS;
+
+double *makeRandSet(int size, int seed){
+	double *set;
+	int i;
+	srand(seed);
+	set = malloc(((size*size) * sizeof(double)));
+	if(set == NULL)
+		return NULL;
+	for(i=0; i<(size*size); i++)
+		set[i] = rand() % 1024;
+	return set;
+}
+
+void *matrixMulti(void *t){
+	int tid = (long)t;
+	int x, y, i;
+
+	int regular_passes = (LENGTH*LENGTH)/THRDS;
+
+	int A_Row = tid / LENGTH;
+	int B_Column = tid % LENGTH; 
+	int index = tid;
+
+	struct timespec start, end;
+	clock_gettime(CLOCK_REALTIME, &start);
+	
+	for(i=0; i<regular_passes+1; i++){
+		if(index < LENGTH*LENGTH){
+	
+			for(x=A_Row*LENGTH, y=B_Column; x<(A_Row+1)*LENGTH; x++, y+=LENGTH)
+				AB[index] += A[x] * B[y];
+			
+			index += THRDS;
+			A_Row = index / LENGTH;
+			B_Column = index % LENGTH;
+		}
+	}
+	clock_gettime(CLOCK_REALTIME, &end);
+	printf("Kernel time is %f\n", (double)((end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec)*1e-9));
+
+	pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[]){
+	
+	int i;
+	LENGTH = atoi(argv[argc-2]);
+	THRDS = atoi(argv[argc-1]);
+
+	A = makeRandSet(LENGTH, time(NULL) - 1);	
+	B = makeRandSet(LENGTH, time(NULL));
+	AB = malloc((LENGTH*LENGTH) * sizeof(double));
+	
+	if(!strcmp(argv[argc-3], "-O")){
+		for(i=0; i<LENGTH*LENGTH; i++){
+			printf("%f ", A[i]);
+			if(i % LENGTH == LENGTH-1)
+				printf("\n");
+		}
+		printf("\n");
+		for(i=0; i<LENGTH*LENGTH; i++){
+			printf("%f ", B[i]);
+			if(i % LENGTH == LENGTH-1)
+				printf("\n");
+		}
+		printf("\n");
+		
+	}
+
+
+	pthread_t threads[THRDS];
+	pthread_attr_t attr;
+	int rc;
+	long t;
+	void *status;
+
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+	for(t=0; t<THRDS; t++){
+		rc=pthread_create(&threads[t], NULL, matrixMulti, (void*)t);
+		if(rc){
+			printf("ERROR: RETURN CODE FROM PTHREAD_CREATE() IS %d\n", rc);
+			exit(-1);
+		}
+	}
+	pthread_attr_destroy(&attr);
+	for(t=0; t<THRDS; t++){
+		rc = pthread_join(threads[t], &status);
+		if(rc){
+			printf("ERROR: RETURN CODE FROM PTHREAD_JOIN() IS %d\n", rc);
+			exit(-1);
+		}
+	}
+	
+	printf("\n");
+	if(!strcmp(argv[argc-3], "-O")){
+		for(i=0; i<LENGTH*LENGTH; i++){
+			printf("%f ", AB[i]);
+			if(i % LENGTH == LENGTH-1)
+				printf("\n");
+		}
+		printf("\n");
+	}
+	
+	free(A);
+	free(B);
+	free(AB);
+
+	pthread_exit(NULL);
+}
